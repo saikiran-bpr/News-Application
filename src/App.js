83bf37fce22b3dfa7f16b8b8c42075599728df79
Bompelliwar Sai Kiran <bompelliwarsaikiran@Sai-Kirans-MacBook-Pro.local>
2024-05-05 02:04:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+  const [category, setCategory] = useState("general");
+  const [newsData, setNewsData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://gnews.io/api/v4/top-headlines?category=${category}&apikey=7dffb3778fb8472a58069c0a2a1ffe03&max=10&lang=en`)
+    .then(response => response.json()).then(data =>{
+      setNewsData(data.articles);
+      setLoading(false);
+    });
+  }, [category]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main">
+      <h1 className='heading'>Top 10 {category} news.</h1>
+      <select value={category} onChange={(e) => {
+        setCategory(e.target.value);
+      }}>
+        <option value="general">General</option>
+        <option value="business">Business</option>
+        <option value="sports">Sports</option>
+        <option value="technology">Technology</option>
+        <option value="world">World</option>
+        <option value="entertainment">Entertainment</option>
+        <option value="science">Science</option>
+      </select>
+      {loading ? <p className='loader'>Loading...</p> : <ol>
+        {newsData.map((it, index) => {
+          return (
+            <li key={index}>
+              <img className='news-img' src={it.image} alt="image"/>
+              <section className='new-title-content-author'>
+                <h3 className='news-title'>{it.title}</h3>
+                <section className='new-content-author'>
+                  <p className='news-description'>{it.description}</p>
+                  <p className='news-source'><strong>Source:</strong> {it.source.name}</p>
+                </section>
+              </section>
+            </li>
+          )
+        })}
+      </ol>}
     </div>
-  );
+  )
 }
+
 
 export default App;
